5de6a491f45525190ea9d18b6f98755f520300ae
Khoa Le <ldakhoa1308@gmail.com>
2020-12-02 18:08:51 +0700

    Add driver and iOS App

---
+++ Sources/Driver/Driver.swift
@@ -0,0 +1,8 @@
+//
+//  Driver.swift
+//  Shiba
+//
+//  Created by Khoa Le on 02/12/2020.
+//
+
+import Foundation
--- Sources/main.swift
+++ Sources/main.swift
@@ -7,4 +7,24 @@
 
 import Foundation
 
-testLexer()
+func populate(driver: Driver, context: ASTContext, input: String) {
+  driver.add("lexing and parsing") { context in
+    var lexer = Lexer(input: input)
+    let tokens = try lexer.lex()
+    let parser = Parser(tokens: tokens, context: context)
+    try parser.parseTopLevel(into: context)
+  }
+}
+
+func main() -> Int32 {
+  let diag = DiagnosticEngine()
+  let context = ASTContext(diagnosticEngine: diag)
+  let driver = Driver(context: context)
+
+  let input = ""
+  populate(driver: driver, context: context, input: input)
+  driver.run(in: context)
+  return diag.hasErrors ? 1 : 0
+}
+
+exit(main())
